ab1f74bf7949a7cbde79f20030673f065d0fa4d7
Piyush Datta <piyush.datta@mail.utoronto.ca>
2017-07-06 02:00:22 -0400

    Made a method called, input_company_db_list, that will input all the companies from either a text file or the sec website onto a db file. It adds in batch sizes of 2000. This seems to be very efficient. May work on a method later on to update the list, which will only insert if a new company is listed or an old company is delisted.

--- SEC_sql_database.py
+++ SEC_sql_database.py
@@ -1,4 +1,3 @@
-
 """
 The SQL database to keep track of CIK keys, Company names, etc. from the SEC website.
 This is the database for pdf_file_scraper.
@@ -14,52 +13,67 @@ from company_information import Company
 conn = sqlite3.connect('sec_info.db')
 conn_cursor = conn.cursor()
 sec_cik_url = requests.get('https://www.sec.gov/Archives/edgar/cik-lookup-data.txt', stream=True)
+our_cik_txt = 'CIK_List.txt'
 
 
-# conn_cursor.execute("""CREATE TABLE Companies (
-#                         line_number integer,
-#                         name text,
-#                         cik_key integer
-#                         )"""
-#                     )
+def create_table():
+    conn_cursor.execute("""CREATE TABLE IF NOT EXISTS companies (
+                            line_number INTEGER,
+                            name TEXT,
+                            cik_key INTEGER
+                            )"""
+                        )
 
-def update_company_db_list():
+
+def input_company_db_list():
+    """
+    Input all the companies from either a text file or the SEC url into
+    the database. Each input includes line number, company name, and company cik key
+
+    :return: None
+    """
     counter = 0
+    batch = list()
+    # Number of companies to dump into db at once
+    batch_size = 2000
+
+    # =================
+    # If using txt file
+    # =================
+    # with open(our_cik_txt) as SEC_listings:
+    #     for line in SEC_listings:
+    # curr_comp_name = str(line.rsplit(':')[0])
+    # curr_comp_cik = str(line.rsplit(':')[-2])
+    # =================
     for line in sec_cik_url.iter_lines():
-        curr_comp_name = str(line)
-        curr_comp_cik = str(line)
+        curr_comp_name = str(line).rsplit(':')[0][2:]
+        curr_comp_cik = str(line).rsplit(':')[-2]
+        counter += 1
+        print(counter, curr_comp_name, curr_comp_cik)
+        # insert_company(counter, curr_comp)
 
-        # Split the line into just the company name
-        curr_comp_name = curr_comp_name.rsplit(':')[0]
-        curr_comp_name = curr_comp_name.split("b", 1)[1]
-        curr_comp_name = curr_comp_name[1:]
+        # Create object of the line
+        batch.append((counter, curr_comp_name, curr_comp_cik))
+        # Insert company is a self-made method, listed below
+        if len(batch) == batch_size:
+            insert_multi_comps(batch)
+            batch = list()
 
-        # Split the line into just the cik number
-        curr_comp_cik = curr_comp_cik.rsplit(':')[-2]
+    # something may be still pending
+    if batch:
+        insert_multi_comps(batch)
 
-        # Create a object out of line
-        curr_comp = Company(curr_comp_name, curr_comp_cik)
+    print("==== Done uploading! ====")
 
-        # Get all rows in database
-        conn_cursor.execute("SELECT * FROM company")
-        all_comp = conn_cursor.fetchall()
 
-        # Check if row corresponds with line, if not, then insert line object
-        try:
-            if all_comp[counter][1] == curr_comp.name and all_comp[counter][-1] == curr_comp.cik \
-                    and all_comp[counter][0] == counter:
-                counter += 1
-            else:
-                counter += 1
-                insert_company(counter, curr_comp)
+def insert_multi_comps(comps):
+    """
 
-        except IndexError:
-            counter += 1
-            insert_company(counter, curr_comp)
-
-        print(counter)
-
-    print("Done updating!")
+    :param comps: List[Integer, String, String]
+    :return: None
+    """
+    with conn:
+        conn_cursor.executemany("INSERT INTO companies VALUES (?, ?, ?)", comps)
 
 
 def insert_company(counter, comp):
@@ -70,7 +84,7 @@ def insert_company(counter, comp):
     :return: None
     """
     with conn:
-        conn_cursor.execute("INSERT INTO company VALUES "
+        conn_cursor.execute("INSERT INTO companies VALUES "
                             "(:line_number, :name, :cik_key)",
                             {'line_number': counter,
                              'name': comp.name,
@@ -84,7 +98,7 @@ def get_company_by_name(comp_name):
     :param comp_name:
     :return: String
     """
-    conn_cursor.execute("SELECT * FROM company WHERE name=:name",
+    conn_cursor.execute("SELECT * FROM companies WHERE name=:name",
                         {'name': comp_name}
                         )
     return conn_cursor.fetchall()
@@ -96,7 +110,8 @@ def get_company_by_cik_key(comp_cik_key):
     :param comp_cik_key: String
     :return: String
     """
-    conn_cursor.execute("SELECT * FROM company WHERE cik_key=:cik_key",
+    conn_cursor.execute("SELECT * FROM companies WHERE "
+                        "cik_key=:cik_key",
                         {'cik_key': comp_cik_key}
                         )
     return conn_cursor.fetchall()
@@ -109,13 +124,13 @@ def delete_all_sql_data():
     Deletes all rows in companies table
     :return: None
     """
-    conn_cursor.execute("DELETE FROM company")
+    conn_cursor.execute("DELETE FROM companies")
     conn.commit()
 
 
 def remove_company(comp):
     with conn:
-        conn_cursor.execute("DELETE from company WHERE "
+        conn_cursor.execute("DELETE from companies WHERE "
                             "name = :name AND "
                             "cik_key = :cik_key",
                             {'name': comp.name,
@@ -124,6 +139,7 @@ def remove_company(comp):
 
 
 if __name__ == '__main__':
-    update_company_db_list()
+    # create_table()
+    # input_company_db_list()
     # delete_all_sql_data()
     conn.close()
